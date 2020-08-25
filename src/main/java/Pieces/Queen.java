74bdb08e6298bfd72ea722b855f538063a7c208b
Federico Maggiore <maggiorefederico@outlook.com>
2020-08-25 16:19:41 +0200

    fixed movement patter for the queen

--- src/main/java/Pieces/Queen.java
+++ src/main/java/Pieces/Queen.java
@@ -1,8 +1,14 @@
 package Pieces;
 
 import src.Board;
+import src.Cell;
+
+import java.util.ArrayList;
 
 public class Queen extends Piece {
+
+    private ArrayList<Cell> availableCells = new ArrayList<>();
+
     public Queen(Boolean white, int x, int y) {
         super("queen", white, x, y);
     }
@@ -19,10 +25,124 @@ public class Queen extends Piece {
                     // moving vertically or horizontally
                     ((distX == 0 && distY !=0) ) ||
                     (distX != 0 && distY ==0)) {
-                if (pieceAtDest != null && this.getWhite() != pieceAtDest.getWhite()) return true;
-                else if (pieceAtDest == null) return true;
-            } else return false;
+                this.checkSurroundings(board);
+                for (Cell cell : availableCells) {
+                    if (cell.getX() == destX && cell.getY() == destY) {
+                        this.availableCells.clear();
+                        return true;
+                    }
+                }
+            }
         } else return false;
         return false;
+    }
+
+    private void checkSurroundings(Board board) {
+        int queenX = this.getxPos();
+        int queenY = this.getyPos();
+        // top left corner
+        for (int i = 1; i <=  queenX && i <= queenY ; i++) {
+            int checkinX = queenX - i;
+            int checkinY = queenY - i;
+            if (board.cellAtIsEmpty(checkinX, checkinY)) this.availableCells.add(board.getCell(checkinX, checkinY));
+            else {
+                Piece piece = board.pieceAtDest(checkinX, checkinY);
+                if (piece.getWhite() != this.getWhite()) {
+                    this.availableCells.add(board.getCell(checkinX, checkinY));
+                    break;
+                } else break;
+            }
+        }
+        // top right corner
+        for (int i = 1; i <= queenX && i < 8 - queenY ; i++) {
+            int checkinX = queenX - i;
+            int checkinY = queenY + i;
+            if (board.cellAtIsEmpty(checkinX, checkinY)) this.availableCells.add(board.getCell(checkinX, checkinY));
+            else {
+                Piece piece = board.pieceAtDest(checkinX, checkinY);
+                if (piece.getWhite() != this.getWhite()) {
+                    this.availableCells.add(board.getCell(checkinX, checkinY));
+                    break;
+                } else break;
+            }
+        }
+        // bottom left corner
+        for (int i = 1; i < 8 - queenX && i <= queenY  ; i++) {
+            int checkinX = queenX + i;
+            int checkinY = queenY - i;
+            if (board.cellAtIsEmpty(checkinX, checkinY)) this.availableCells.add(board.getCell(checkinX, checkinY));
+            else {
+                Piece piece = board.pieceAtDest(checkinX, checkinY);
+                if (piece.getWhite() != this.getWhite()) {
+                    this.availableCells.add(board.getCell(checkinX, checkinY));
+                    break;
+                } else break;
+            }
+        }
+        // bottom right corner
+        for (int i = 1; i < 8 - queenX && i < 8 - queenY ; i++) {
+            int checkinX = queenX + i;
+            int checkinY = queenY + i;
+            if (board.cellAtIsEmpty(checkinX, checkinY)) this.availableCells.add(board.getCell(checkinX, checkinY));
+            else {
+                Piece piece = board.pieceAtDest(checkinX, checkinY);
+                if (piece.getWhite() != this.getWhite()) {
+                    this.availableCells.add(board.getCell(checkinX, checkinY));
+                    break;
+                } else break;
+            }
+        }
+        // moving up
+        for (int i = 1; i <=  queenX; i++) {
+            int checkinX = queenX - i;
+            int checkinY = queenY;
+            if (board.cellAtIsEmpty(checkinX, checkinY)) this.availableCells.add(board.getCell(checkinX, checkinY));
+            else {
+                Piece piece = board.pieceAtDest(checkinX, checkinY);
+                if (piece.getWhite() != this.getWhite()) {
+                    this.availableCells.add(board.getCell(checkinX, checkinY));
+                    break;
+                } else break;
+            }
+        }
+        // moving right
+        for (int i = 1;  i < 8 - queenY ; i++) {
+            int checkinX = queenX;
+            int checkinY = queenY + i;
+            if (board.cellAtIsEmpty(checkinX, checkinY)) this.availableCells.add(board.getCell(checkinX, checkinY));
+            else {
+                Piece piece = board.pieceAtDest(checkinX, checkinY);
+                if (piece.getWhite() != this.getWhite()) {
+                    this.availableCells.add(board.getCell(checkinX, checkinY));
+                    break;
+                } else break;
+            }
+        }
+        // moving left
+        for (int i = 1; i <= queenY  ; i++) {
+            int checkinX = queenX;
+            int checkinY = queenY - i;
+            if (board.cellAtIsEmpty(checkinX, checkinY)) this.availableCells.add(board.getCell(checkinX, checkinY));
+            else {
+                Piece piece = board.pieceAtDest(checkinX, checkinY);
+                if (piece.getWhite() != this.getWhite()) {
+                    this.availableCells.add(board.getCell(checkinX, checkinY));
+                    break;
+                } else break;
+            }
+        }
+        // moving down
+        for (int i = 1; i < 8 - queenX ; i++) {
+            int checkinX = queenX + i;
+            int checkinY = queenY;
+            if (board.cellAtIsEmpty(checkinX, checkinY)) this.availableCells.add(board.getCell(checkinX, checkinY));
+            else {
+                Piece piece = board.pieceAtDest(checkinX, checkinY);
+                if (piece.getWhite() != this.getWhite()) {
+                    this.availableCells.add(board.getCell(checkinX, checkinY));
+                    break;
+                } else break;
+            }
+        }
     }
 }
